d0e6041895159c23dc3a7e416ab3eb239ae771bf
smurak <smurak000@gmail.com>
2025-06-15 19:10:24 +0900

    chore: bump version to 0.1.25

--- src/easybench/__init__.py
+++ src/easybench/__init__.py
@@ -8,7 +8,7 @@ from .core import BenchConfig, BenchParams, EasyBench, customize, fixture, param
 from .decorator import bench
 from .utils import get_bench_env
 
-__version__ = "0.1.24"
+__version__ = "0.1.25"
 
 __all__ = [
     "BenchConfig",
